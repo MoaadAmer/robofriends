56afd6d4f93cdffbe57ea17277f6954b9dc1cbbb
Moaad Amer <moaad02@gmail.com>
2025-04-21 05:49:34 +0300

    fetch the robots from network

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import "./App.css";
-import { robots } from "./robots.js";
 import CardList from "./CardsList.jsx";
 import SearchBox from "./SearchBox.jsx";
 import { Component } from "react";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
+      robots: [],
       searchField: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((jsonArray) => this.setState({ robots: jsonArray }));
   }
 
   onSearchFieldChange = (event) => {
@@ -17,23 +23,27 @@ class App extends Component {
   };
 
   render() {
-    const fileredRobots = robots.filter((r) => {
-      return r.name
-        .toLowerCase()
-        .startsWith(this.state.searchField.toLowerCase());
-    });
-    return (
-      <>
-        <h1>Robofriends</h1>
-        <SearchBox
-          placeholder="Enter Robot Name"
-          onChange={this.onSearchFieldChange}
-        />
-        <div className="container">
-          <CardList robots={fileredRobots} />
-        </div>
-      </>
-    );
+    if (this.state.robots.length === 0) {
+      return <p>Loading</p>;
+    } else {
+      const fileredRobots = this.state.robots.filter((r) => {
+        return r.name
+          .toLowerCase()
+          .startsWith(this.state.searchField.toLowerCase());
+      });
+      return (
+        <>
+          <h1>Robofriends</h1>
+          <SearchBox
+            placeholder="Enter Robot Name"
+            onChange={this.onSearchFieldChange}
+          />
+          <div className="container">
+            <CardList robots={fileredRobots} />
+          </div>
+        </>
+      );
+    }
   }
 }
 
